f4068b0cb2a691dc79f306a8759d3a0e6ef02a81
Ion Cioca <ciocaion2298@gmail.com>
2023-02-08 18:08:39 +0100

    Added framer-motion and animations

--- src/components/library/buttons/IconButton.tsx
+++ src/components/library/buttons/IconButton.tsx
@@ -1,24 +1,43 @@
 import React from 'react';
+import { motion } from "framer-motion";
 import { Bookmark } from 'lucide-react';
 import { Heart } from 'lucide-react';
 import { ThumbsUp } from 'lucide-react';
 import styles from './IconButton.module.css';
 
 interface Props {
+  /**
+   * Callback function that is called when the button is clicked.
+   */
   onClick: () => void;
+   /**
+   * The name of the icon to display in the button.
+   * values are: 'bookmark', 'heart', 'thumbsup'
+   */
   icon: string;
+  /**
+   * Whether the button is enabled or not.Use true, false or enable.
+   */
   enabled: boolean;
   fill: string;
   color: string;
 }
-
+/**
+ * A button with an icon.
+ */
 const IconButton: React.FC<Props> = ({ onClick, icon,enabled, fill, color }) => {
   return (
-    <button className={styles.iconButton} onClick={onClick}>
+    
+    <motion.button
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+      className={styles.iconButton} 
+      onClick={onClick}
+    >
       {icon === 'bookmark' && <Bookmark color={color} fill={fill} />}
       {icon === 'heart' && <Heart color={color} fill={fill} />}
       {icon === 'thumbsup' && <ThumbsUp color={color} fill={fill} />}
-    </button>
+    </motion.button>
   );
 };
 
